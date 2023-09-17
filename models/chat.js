55564dbd843d75d98bb2e271853380a0206044eb
Ashikur Rahman <ashikmoinkhan@gmail.com>
2023-09-17 16:38:08 +0600

    added post-report API

--- models/chat.js
+++ models/chat.js
@@ -4,11 +4,9 @@ const chatSchema = mongoose.Schema(
     {
         members: {
           type: Array,
+          required: true
         },
       },
-      {
-        timestamps: true,
-      }
 );
 
 const Chat = mongoose.model('Chat', chatSchema);
